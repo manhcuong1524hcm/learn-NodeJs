3a9bfcb2f637b86d2c837d4fcf499700cc0d79c2
ManhCuong <manhcuong1524hcm@gmail.com>
2022-03-09 18:12:52 +0700

    morgan

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/',  (req, res) => res.send('Lâm Mạnh Cường'))
+app.use(morgan('combined'))
+
+app.get('/',  (req, res) => res.send('Mạnh Cường'))
 
 
 
